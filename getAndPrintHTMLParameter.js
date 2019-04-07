74dcd39b81e9c200d898f97f08ede5329a9a231b
minlucyzhao <minlucyzhao@gmail.com>
2019-04-07 22:45:04 +0000

    HTTPS client to retreive and print HTML data from the internet with parameter

---
+++ getAndPrintHTMLParameter.js
@@ -0,0 +1,21 @@
+var https = require('https');
+
+function getAndPrintHTML(options) {
+    https.get(requestOptions, function(response) {
+        var appendedData = "";
+        response.on('data', function(data) {
+                appendedData += data;
+        });
+
+        response.on('end', function() {
+            console.log(appendedData);
+        });
+    });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
